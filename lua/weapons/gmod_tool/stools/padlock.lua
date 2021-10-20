316ef7571c7d082100b140e6d8fe037e0eb260ef
vurtual <VurtualRuler98@gmail.com>
2021-10-20 15:42:57 -0500

    rope and two placement options

--- lua/weapons/gmod_tool/stools/padlock.lua
+++ lua/weapons/gmod_tool/stools/padlock.lua
@@ -1,6 +1,12 @@
 TOOL.Category = "Constraints"
 TOOL.Name = "Padlock"
 function TOOL:LeftClick(trace)
+	return self:Padlock(trace,false)
+end
+function TOOL:RightClick(trace)
+	return self:Padlock(trace,true)
+end
+function TOOL:Padlock(trace,rightclick)
 	if (IsValid(trace.Entity) and trace.Entity:IsPlayer()) then return end
 	if (SERVER and not util.IsValidPhysicsObject(trace.Entity,trace.PhysicsBone)) then return end
 	local phys = trace.Entity:GetPhysicsObjectNum(trace.PhysicsBone)
@@ -12,19 +18,38 @@ function TOOL:LeftClick(trace)
 	end
 	self:SetOperation(1)
 	if (inum == 0) then
-		self:SetStage(1)
+		if (rightclick) then
+			self:SetStage(2)
+		else
+			self:SetStage(1)
+		end
 		return true
 	end
 	if (inum == 1) then
 		local ent1,ent2=self:GetEnt(1),self:GetEnt(2)
 		local bone1,bone2=self:GetBone(1),self:GetBone(2)
-		local tiedown=constraint.Weld(ent1,ent2,bone1,bone2,0,false)
+		local tiedown
+		if (self:GetStage()==1) then
+			tiedown=constraint.Weld(ent1,ent2,bone1,bone2,0,false)
+		end
+		if (self:GetStage()==2) then
+			tiedown=constraint.Rope(ent1,ent2,bone1,bone2,self:GetLocalPos(1),self:GetLocalPos(2),(self:GetPos(1)-self:GetPos(2)):Length(),0,0,1,"cable/rope",false,Color(255,255,255,255))
+		end
 		local padlock = ents.Create("sent_tiedown_padlock")
 		local offset=Vector(trace.Normal)
+		offset:Rotate(Angle(-90,0,0))
+		offset=offset*5
+		local ang=trace.Normal:Angle()
+		ang=ang:SnapTo("x",360):SnapTo("y",360)
+		if (not rightclick) then
+			offset=offset-trace.Normal*2
+		end
+		if (rightclick) then
+			offset=offset+trace.Normal*2
+		end
 		if (tiedown) then
-			offset:Rotate(Angle(90,0,0))
-			padlock:SetPos(trace.HitPos+trace.Normal*2+offset*5) --test
-			padlock:SetAngles(trace.Normal:Angle())
+			padlock:SetPos(trace.HitPos+offset) --test
+			padlock:SetAngles(ang)
 			padlock:Spawn()
 			local padlockweld=constraint.Weld(padlock,ent2,0,bone2,0,true)
 			if (padlock and padlockweld) then
